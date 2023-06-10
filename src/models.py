5e4ba62b3c706ccb5d33b4f42063b8bc08f70373
Andres Zabala <andresjzabalac@hotmail.com>
2023-06-10 15:19:36 +0000

    a

--- src/models.py
+++ src/models.py
@@ -5,6 +5,8 @@ from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine, UniqueConstraint
 from eralchemy2 import render_er
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 
 Base = declarative_base()
 
@@ -23,6 +25,8 @@ class User(Base):
     password = Column('password', String(40), nullable=False)
     email = Column('email', String(100), nullable=False, unique=True)
     status = Column('status', Enum(UserStatus), nullable=False, default=str(UserStatus.invalid))
+    created_at = Column('created_at', TIMESTAMP(True), default=text('now()'))
+    update_at = Column('updated_at', TIMESTAMP(True))
 
     # relationship
     favorites = relationship('Favorite', back_populates='users')
@@ -34,41 +38,31 @@ class User(Base):
         pass
 
 class Gender(enum.Enum):
-    male = 0
-    woman = 1
-    noDefined = 2
+    male = 'male'
+    woman = 'woman'
+    noDefined = 'noDef'
 
-class ItemType(enum.Enum):
-    character = 0
-    planet = 1
-
-# class Item(Base):
-#     __tablename__ = 'items'
-
-#     # attributes
-#     id = Column('id', Integer, primary_key=True)
-#     idCharacter = Column('idCharacter', Integer, unique=True)
-#     idPlanet = Column('idPlanet', Integer, unique=True)
-
-#     # relationship
-#     characters = relationship('Character', back_populates='items')
-#     planets = relationship('Planet', back_populates='items')
+class Species(enum.Enum):
+    human = 'human'
+    wookie = 'wookie'
+    robot = 'robot'
 
 class Character(Base):
     __tablename__ = 'characters'
 
     # attributes
     id = Column('id', Integer, primary_key=True)
-    itemId = Column('itemId', Integer, ForeignKey('items.id'))
+    item_id = Column('item_id', Integer, ForeignKey('items.id'))
     name = Column('name', String(100), nullable=False)
     height = Column('height', Integer, nullable=False)
     mass = Column('mass', Integer, nullable=False)
-    hairColor = Column('hairColor', String(30))
-    skinColor = Column('skinColor', String(30), nullable=False)
-    birthYear = Column('birthYear', String(30), nullable=False)
-    gender = Column('gender', Enum(Gender), default=str(Gender.noDefined))
-    homeWorld = Column('homeWorld', Integer, nullable=False)    #Esto sera una foreing
-    species = Column('species', Integer, nullable=False)        #Esto sera un foreign
+    hair_color = Column('hair_color', String(30))
+    skin_color = Column('skin_color', String(30), nullable=False)
+    birth_Year = Column('birth_year', String(30), nullable=False)
+    gender = Column('gender', Enum(Gender), default=(Gender.noDefined))
+    species = Column('species', Enum(Species), default=(Species.human))
+    created_at = Column('created_at', TIMESTAMP(True), default=text('now()'))
+    update_at = Column('updated_at', TIMESTAMP(True))
     
     # relationship
     item = relationship('Item', back_populates='characters')
@@ -78,37 +72,38 @@ class Planet(Base):
 
     # atributtes
     id = Column('id', Integer, primary_key=True)
-    itemId = Column('itemId', ForeignKey('items.id'))
+    item_id = Column('item_id', ForeignKey('items.id'))
     name = Column('name', String(100), nullable=False)
-    rotationPeriod = Column('rotationPeriod', Integer, nullable=False)
+    rotation_period = Column('rotation_period', Integer, nullable=False)
     diameter = Column('diameter', Integer, nullable=False)
     climate = Column('climate', String(50), nullable=False)
     gravity = Column('gravity', String(50), nullable=False)
     terrain = Column('terrain', String(50), nullable=False)
-    superfaceWater = Column('superfaceWater', Integer, nullable=False)
+    superface_water = Column('superface_water', Integer, nullable=False)
     population = Column('population', Integer, nullable=False)
+    created_at = Column('created_at', TIMESTAMP(True), default=text('now()'))
+    update_at = Column('updated_at', TIMESTAMP(True))
 
     # relationship
     item = relationship('Item', back_populates='planets')
+
+class Naturaleza(enum.Enum):
+    people = 'people'
+    planet = 'planet'
 
 class Favorite(Base):
     __tablename__ = 'favorites'
 
     # attributes
     id = Column('id', Integer, primary_key=True)
-    userId = Column('userId', ForeignKey('users.id'), nullable=False)
-    # itemId = Column('itemId', ForeignKey('items.id'), nullable=False)
-    idCharacter = Column('idCharacter', Integer, ForeignKey('characters.id'), unique=True)
-    idPlanet = Column('idPlanet', Integer, ForeignKey('planets.id'), unique=True)
+    user_id = Column('user_id', ForeignKey('users.id'), nullable=False)
+    naturaleza = Column('naturaleza', Enum(Naturaleza), nullable=False)
+    naturaleza_id = Column('naturaleza_id', Integer, ForeignKey('planets.id'), ForeignKey('characters.id'))
 
 #     # relationship
-    characters = relationship('Character', back_populates='items')
-    planets = relationship('Planet', back_populates='items')
-
-
-    # relationships
+    characters = relationship('Character', back_populates='favorites')
+    planets = relationship('Planet', back_populates='favorites')
     user = relationship('User', back_populates='favorites')
-    # item = relationship('Item', back_populates='favorites')
 
 
 ## Draw from SQLAlchemy base
